9afeba256285ee527395639f17c4555bcc814592
UrFrM <70636113+UrFrM@users.noreply.github.com>
2020-09-02 23:27:13 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
-const nombres = document.getElementById("nombres")
+const nombre = document.getElementById("name")
 const apellidos = document.getElementById("apellidos")
-const email = document.getElementById("correo")
-const pass = document.getElementById("contraseña")
-const section = document.getElementById("form")
+const email = document.getElementById("email")
+const pass = document.getElementById("password")
+const form = document.getElementById("form")
 const parrafo = document.getElementById("warnings")
 
 form.addEventListener("submit", e=>{
   e.preventDefault()
   let warnings = ""
   let entrar = false
-  let regexEmail = /^\w+([\.-]?\w+)*@\w+(\.-]?\w+)*(\.\w{2,4})+$/
-  parrafo.innerHTML =""
-  if(nombres.Value.length <3){
+  let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
+  parrafo.innerHTML = ""
+  if(nombre.value.length <3){
     warnings += 'El nombre no es valido <br>'
+    entrar = true
+  }
+  if(apellidos.value.length <3){
+    warnings += 'El apellido no es valido <br>'
     entrar = true
   }
   if(!regexEmail.test(email.value)){
@@ -26,8 +30,8 @@ form.addEventListener("submit", e=>{
   }
   
   if(entrar){
-    parrafo.index.html = warnings
+    parrafo.innerHTML = warnings
   }else{
-    parrafo.index.html = "Enviado"
+    parrafo.innerHTML = "Enviado"
   }
 })
